8471a6f805b8ec56bf13b342509375d8e19b568c
nswartwo <nswartwo@gmail.com>
2023-10-02 17:44:48 -0400

    Update costs to 2021 USD.

--- R/HealthServiceUnitCosts.R
+++ R/HealthServiceUnitCosts.R
@@ -14,19 +14,31 @@ HealthServiceUnitCosts <- function(){
                                  'TBtx')
   ##LTBI Care Cascade Associated Costs
   DefaultCostsVector['LTBIIdCost'] <- 0
-  DefaultCostsVector['TSTCost'] <- 9.38
-  DefaultCostsVector['IGRACost'] <- 61.98
-  DefaultCostsVector['NoTBCost'] <- 33.20
+  DefaultCostsVector['TSTCost'] <- 10.12 ### CMS 2021, Physician Fee CPT 86580
+  DefaultCostsVector['IGRACost'] <- 61.98 ### CMS 2021, Clinical Lab Fee CPT 86480
+  DefaultCostsVector['NoTBCost'] <- 34.20 ### CMS 2021, Physician’s Fee CPT 71046
+
 
   ## LTBI Treatment Regimens Costs
-  DefaultCostsVector['3HPCost'] <- 411.87
-  DefaultCostsVector['4RCost'] <- 354.87
-  DefaultCostsVector['3HRCost'] <- 353.61
+  ### for ease of update, we will define the daily cost of each drug
+  ### then compute the combo below for the regimen
+  ### we also include clinic cost, supplies, and adverse events clinic cost
+  rifampinCost    <- 2.45 # rifampin 600 mg, $2.45/dose;
+  isoniazidCost   <- 0.19 # isoniazid 300 mg, $0.19/dose;
+  rifapentineCost <- 14.44 * (900/1200) #rifapentine 1,200 mg, $14.44/dose;
+
+  ### 3HP = 3 months of weekly doeses of 900 isoniazid and 900 mg of rifapentine
+  DefaultCostsVector['3HPCost'] <- 12 * (rifapentineCost + 3*isoniazidCost) + 176 + 68 + 34
+  ### 4R = 4 months of daily doses of 600 rifampin
+  DefaultCostsVector['4RCost'] <- 120 * rifampinCost + 203 + 72 + 11
+  ### 3HR = 3 months of daily doses of 300 isoniazid and 600 mg of rifampin
+  DefaultCostsVector['3HRCost'] <- 90 * (rifampinCost + isoniazidCost) + 176 + 68 + 34
+  DefaultCostsVector['3HRCost'] <- 353.61 ### Still 2020
 
   ## TB Care Cascade Costs
   DefaultCostsVector['TBIdCost'] <- 0
   DefaultCostsVector['TBtest'] <- 0
-  DefaultCostsVector['TBtx'] <- 20211
+  DefaultCostsVector['TBtx'] <- 23060 ### EID Publication in press 2023
 
   return(DefaultCostsVector)
 }
--- R/ProductivityCosts.R
+++ R/ProductivityCosts.R
@@ -13,66 +13,68 @@ calculateProdCost <- function(TbCases,
                               tx_dist = c(1/3, 1/3, 1/3),
                               Ages="SingleYearAge"){
 
-##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### #####
-## PRODUCTIVITY COST CALCULATION VALUES
-##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### #####
-TLTBI_clinic<-48.01+28.16 #initial and follow-up clinic visits
-TLTBI_ae<-5.27 #adverse event costs with 3HP or 3HR
-P_TB_hosp<-0.49 #probability of hospitalization with TB
-DUR_TB_prTX <- 3/12 # average duration of TB prior to treatment (3 months)
-DUR_TB_TX_hosp<-24/365 #average duration of hospitalization with TB (24 days)
-DUR_TB_TX_outpatient <- 6.8/365 #duration of time loss from outpatient services (6.8 days)
-P_TLTBI_tox <- .003*(tx_dist[1]+tx_dist[2]) #CDC based
+  ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### #####
+  ## PRODUCTIVITY COST CALCULATION VALUES
+  ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### #####
+  TLTBI_clinic <- 48.01+28.16 #initial and follow-up clinic visits
+  TLTBI_ae <- 5.27 #adverse event costs with 3HP or 3HR
+  P_TB_hosp <- 0.49 #probability of hospitalization with TB
+  DUR_TB_prTX <- 3*30 # average duration of TB prior to treatment (3 months)
+  DUR_TB_TX_hosp <- 24/365 #average duration of hospitalization with TB (24 days)
+  DUR_TB_TX_outpatient <- 6.8/365 #duration of time loss from outpatient services (6.8 days)
+  P_TLTBI_tox <- .003*(tx_dist[1]+tx_dist[2]) #CDC based
 
-##### Bureau of Labor 2016 to 2020 conversion value
-convFact <- 1.14
+  ##### Bureau of Labor 2016 to 2020 conversion value
+  # convFact <- 1.14
+  ##### Bureau of Labor 2016 to 2021 conversion value
+  convFact <- 1.245
 
-## Check for discount and add in appropriate data
-if (discount == 0){
+  ## Check for discount and add in appropriate data
+  if (discount == 0){
     discountVec <- rep(1, nrow(TbCases))
     annual_prod <- readRDS(system.file(paste0("AnnualProductivity", Ages, ".rds"), package = "TubercuCost"))*convFact
-    annual_expend <- readRDS(system.file(paste0("NonHealthExpenditure", Ages, ".rds"), package = "TubercuCost")) # need to convert to 2020 dollars in csv and formatting
+    annual_expend <- readRDS(system.file(paste0("NonHealthExpenditure", Ages, ".rds"), package = "TubercuCost")) #converted to 2021 in csv
     lifetime_prod <- readRDS(system.file(paste0("LifetimeProductivityCosts", Ages, ".rds"), package = "TubercuCost"))*convFact
     lifetime_expend <- readRDS(system.file(paste0("LifetimeNonHealthExpenditure", Ages, ".rds"), package = "TubercuCost"))
-} else if (discount == 0.03){
+  } else if (discount == 0.03){
     discountVec <- read.csv("~/TubercuCost/inst/extdata/DiscountingThreePercentScalars.csv")[1:nrow(TbCases),2]
     annual_prod <- readRDS(system.file(paste0("DiscountedAnnualProductivityCosts", Ages, ".rds"), package = "TubercuCost"))*convFact
-    annual_expend <- readRDS(system.file(paste0("DiscountedNonHealthExpenditure", Ages, ".rds"), package = "TubercuCost")) # need to convert to 2020 dollars in csv and formatting
+    annual_expend <- readRDS(system.file(paste0("DiscountedNonHealthExpenditure", Ages, ".rds"), package = "TubercuCost")) #converted to 2021 in csv
     lifetime_prod <- readRDS(system.file(paste0("DiscountedLifetimeProductivityCosts", Ages, ".rds"), package = "TubercuCost"))*convFact
     lifetime_expend <- readRDS(system.file(paste0("DiscountedLifetimeNonHealthExpenditure", Ages, ".rds"), package = "TubercuCost"))
-} else {
+  } else {
     print("Current discount level not supported.")
     break()
-}
-
-##### CREATE A DATAFRAME OF COSTS, NOT THE TOTAL SUM
-##### WE CAN CREATE SUMS ON OTHER SIDE
-  TbMortCost <- TltbiProdCost <- TbProdCost <-matrix(0, nrow = nrow(TbCases),
-                                                        ncol = ncol(TbCases))
-for (i in 1:nrow(TbCases)){
-  for (j in 1:ncol(TbCases)){
-    ##### PRODUCTIVITY COSTS DUE TO TLTBI
-    ##### ##### we divide by 1e3 to get the costs in millions (counts are in 000s when read in)
-    # number of initiations * clinic visits cost * adverse event costs
-    # TltbiProdCost[i,j] <- (LtbiTxInits[i,j] *(TLTBI_clinic+TLTBI_ae*P_TLTBI_tox)*discountVec[i])
-
-    #PRODUCTIVITY COSTS DUE TO TB DISEASE
-    #age specific TB treatment initiations*((probability of TB hospitalization*duration of TB hospitalization)+
-    #duration of outpatient losses)*annual productivity estimates +
-    #age specific TB deaths*lifetime productivity estimates
-    TbProdCost[i,j] <- (TbCases[i,j]*(annual_prod[i,j] - annual_expend[i,j]))*((DUR_TB_prTX + P_TB_hosp*DUR_TB_TX_hosp)+DUR_TB_TX_outpatient) * discountVec[i]
-    TbMortCost[i,j] <- (TbDeaths[i,j]*(lifetime_prod[i,j]-lifetime_expend[i,j]))
   }
-}
-colnames(TltbiProdCost) <- colnames(TbProdCost) <- colnames(TbCases)
-ProdCosts <- list()
 
-ProdCosts[["TltbiProdCost"]] <- TltbiProdCost
-ProdCosts[["TbProdCost"]]    <- TbProdCost
-ProdCosts[["TbMortCost"]]    <- TbMortCost
+  ##### CREATE A DATAFRAME OF COSTS, NOT THE TOTAL SUM
+  ##### WE CAN CREATE SUMS ON OTHER SIDE
+  TbMortCost <- TltbiProdCost <- TbProdCost <-matrix(0, nrow = nrow(TbCases),
+                                                     ncol = ncol(TbCases))
+  for (i in 1:nrow(TbCases)){
+    for (j in 1:ncol(TbCases)){
+      ##### PRODUCTIVITY COSTS DUE TO TLTBI
+      ##### ##### we divide by 1e3 to get the costs in millions (counts are in 000s when read in)
+      # number of initiations * clinic visits cost * adverse event costs
+      # TltbiProdCost[i,j] <- (LtbiTxInits[i,j] *(TLTBI_clinic+TLTBI_ae*P_TLTBI_tox)*discountVec[i])
 
-ProdCosts[["TotalProdCost"]] <- TbProdCost + TbMortCost + TltbiProdCost
+      #PRODUCTIVITY COSTS DUE TO TB DISEASE
+      #age specific TB treatment initiations*((probability of TB hospitalization*duration of TB hospitalization)+
+      #duration of outpatient losses)*annual productivity estimates +
+      #age specific TB deaths*lifetime productivity estimates
+      TbProdCost[i,j] <- (TbCases[i,j]*(annual_prod[i,j] - annual_expend[i,j]))*((DUR_TB_prTX + P_TB_hosp*DUR_TB_TX_hosp)+DUR_TB_TX_outpatient) * discountVec[i]
+      TbMortCost[i,j] <- (TbDeaths[i,j]*(lifetime_prod[i,j]-lifetime_expend[i,j]))
+    }
+  }
+  colnames(TltbiProdCost) <- colnames(TbProdCost) <- colnames(TbCases)
+  ProdCosts <- list()
 
-return(ProdCosts)
+  ProdCosts[["TltbiProdCost"]] <- TltbiProdCost
+  ProdCosts[["TbProdCost"]]    <- TbProdCost
+  ProdCosts[["TbMortCost"]]    <- TbMortCost
+
+  ProdCosts[["TotalProdCost"]] <- TbProdCost + TbMortCost + TltbiProdCost
+
+  return(ProdCosts)
 
 }
